c54802e30f56997951a00063063b40ade6805e4a
Tadaboody <tomerpet@gmail.com>
2018-04-21 22:58:11 +0300

    Expand pprefix.c testing range by using long long

--- pprefix.c
+++ pprefix.c
@@ -3,24 +3,25 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
-typedef struct size_t_container
+typedef long long index_t;
+typedef struct index_t_container
 {
-    size_t data;
+    index_t data;
 } data_t;
 typedef void* generic_p;
-#define N 100
+#define N 10000000
 typedef bool (*predicate)(generic_p);
 typedef struct filter_ret_t
 {
     generic_p filtered_array;
-    size_t filtered_array_len;
+    index_t filtered_array_len;
 }filter_ret_t;
 
-size_t *prefix_sum(size_t *array, size_t n)
+index_t *prefix_sum(index_t *array, index_t n)
 {
-    size_t *x = malloc(sizeof(data_t) * n);
-    size_t *t = malloc(sizeof(data_t) * n);
-    size_t i,j;
+    index_t *x = malloc(sizeof(data_t) * n);
+    index_t *t = malloc(sizeof(data_t) * n);
+    index_t i,j;
     #pragma omp parallel for private(i)
     for (i = 0; i < n; i++)
         x[i] = array[i];
@@ -40,25 +41,25 @@ size_t *prefix_sum(size_t *array, size_t n)
     return x;
 }
 
-filter_ret_t filter(generic_p *array, size_t length, predicate p)
+filter_ret_t filter(generic_p *array, index_t length, predicate p)
 {
-    size_t *bitmap, *bitsum;
+    index_t *bitmap, *bitsum;
     generic_p *filtered;
-    bitmap = malloc(sizeof(generic_p) * length);
+    bitmap = malloc(sizeof(index_t) * length);
     #pragma omp parralel for
-    for(int i=0;i<length;i++)
+    for(index_t i=0;i<length;i++)
     {
         bitmap[i] = p(array[i]);
     }
     bitsum = prefix_sum(bitmap, length);
-    size_t filtered_length = bitsum[length - 1];
+    index_t filtered_length = bitsum[length - 1];
     filtered = malloc(sizeof(generic_p) * filtered_length);
     if(bitsum[0] > 0) // edge index
     {
         filtered[0] = array[0];
     }
     #pragma omp parallel for
-    for (int i = 1; i < length; i++)
+    for (index_t i = 1; i < length; i++)
     {
         if(bitsum[i] > bitsum[i-1])
         {
@@ -80,14 +81,14 @@ bool even(generic_p a)
 void test_psum()
 {
     bool assert_psum = true;
-    size_t *a = malloc(sizeof(size_t) * N);
+    index_t *a = malloc(sizeof(index_t) * N);
     for (long i = 0; i < N; i++)
         a[i] = i + 1;
-    size_t *a_psum = prefix_sum(a, N);
+    index_t *a_psum = prefix_sum(a, N);
     #pragma omp parallel for reduction(&: assert_psum)
-    for (size_t i = 1; i <= N; i++)
+    for (index_t i = 1; i <= N; i++)
     {
-        size_t expected = (i * (i + 1)) / 2;
+        index_t expected = (i * (i + 1)) / 2;
         bool current_test = (a_psum[i - 1] == expected);
         if (!current_test)
         {
@@ -103,17 +104,17 @@ void test_psum()
 void test_filter()
 {
     data_t **a = malloc(sizeof(data_t *) * N);
-    for (size_t i = 0; i < N; i++)
+    for (index_t i = 0; i < N; i++)
     {
         a[i] = malloc(sizeof(data_t));
         a[i]->data = i + 1;
     }
     filter_ret_t a_filtered = filter((generic_p *)a, N, even);
     data_t **a_filterd_array = a_filtered.filtered_array;
-    size_t filtered_length = a_filtered.filtered_array_len;
+    index_t filtered_length = a_filtered.filtered_array_len;
     bool assert_filter = true;
     #pragma omp parallel for reduction(&& : assert_filter)
-    for (size_t i = 0; i < filtered_length; i++)
+    for (index_t i = 0; i < filtered_length; i++)
     {
         data_t expected = {2 * (i+1)};
         data_t *actual = a_filterd_array[i];
@@ -126,7 +127,7 @@ void test_filter()
     }
     printf("assert_filter=%s\n", assert_filter ? "True" : "False");
     free(a_filterd_array);
-    for (size_t i = 0; i < N; i++)
+    for (index_t i = 0; i < N; i++)
     {
         free(a[i]);
     }
